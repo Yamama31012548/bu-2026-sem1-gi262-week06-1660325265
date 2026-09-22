fe1c880cbdcb614c12f8238b84deafcd26a99eff
Yamama31012548 <bong31012548@gmail.com>
2026-09-22 13:04:36 +0700

    Exterm

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.PlayerLoop;
 
 namespace MidtermExam.Prob01
 {
     public class GameEntity
     {
-        // TODO: Implement fields and methods according to Class Diagram
+        public string id;
+        private Vector3 position;
+        protected int health;
+        public virtual void Update()
+        {
+
+        }
+        public virtual void TakeDamage(int damage)
+        {
+          health = damage;
+        }
+        private void Move(Vector3 direction)
+        {
+            position += direction;
+        }
     }
 }
